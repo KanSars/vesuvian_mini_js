c57e99d71ee872f00644c3ea21f4df704387cb68
Нахмурин Кирилл Алексеевич <nahmurinka@MacBook-Air-Nahmurin.local>
2026-05-20 16:41:57 +0400

    add learn javascript course cards through advanced sections

--- src/features/collections/jsCourseView/model/courseCards/index.ts
+++ src/features/collections/jsCourseView/model/courseCards/index.ts
@@ -1,8 +1,12 @@
 import { Card } from 'entities/Card/model/types';
 import { introCards } from './intro';
 import { fundamentalsCards } from './fundamentals';
+import { objectsBasicsCards } from './objectsBasics';
+import { languageAdvancedCards } from './languageAdvanced';
 
 export const courseCards: Record<string, Card[]> = {
   ...introCards,
   ...fundamentalsCards,
+  ...objectsBasicsCards,
+  ...languageAdvancedCards,
 };
